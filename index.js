a0eff3a6d6990b9f3fbb0a29a01e299d80490703
Imani William <iawilli4@ncsu.edu>
2016-10-29 17:58:35 -0400

    updated the bot to speak

--- index.js
+++ index.js
@@ -28,3 +28,41 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+//API Endpoint to process messages
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAEnfFiEp68BAFZAumeQ1YaJi92AZBhTrhAwrRMw4wtTztD2qj3ONozV7m5h7lJdVFpjTaHq2WMZBZCFIgkLH3qKaClZB9Soi1ZAWYUgr2yfYb6Y2Fx3bO8HKnQvHmMnZCnCQVmYxGEiVNti5cJZCzey9P3lBrG3FapXdanVwaRSrAZDZD"
+
+//function to echo back messages
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
